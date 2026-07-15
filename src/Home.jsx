bbaa675c5e711b555bc0befc41ee8e2cc06a87eb
Udhayakumar M <udhayakumar2969@gmail.com>
2026-07-15 11:07:22 +0530

    Home page created

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+
+export function Home(){
+    return(
+        <>
+        <p>User Details </p>
+        {/* <h2>name:{props.name}</h2>
+        <h3>age:{props.age}</h3>
+        <h4>city:{props.city}</h4> */}
+        </>
+    )
+}
