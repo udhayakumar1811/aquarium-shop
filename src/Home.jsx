c2d7838bd813c5df9d36aec51ad04637ef2c60a2
Udhayakumar M <udhayakumar2969@gmail.com>
2026-07-15 11:33:38 +0530

    Home page updated props

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,9 +7,9 @@ export function Home(){
     return(
         <>
         <p>User Details </p>
-        {/* <h2>name:{props.name}</h2>
+        <h2>name:{props.name}</h2>
         <h3>age:{props.age}</h3>
-        <h4>city:{props.city}</h4> */}
+        <h4>city:{props.city}</h4>
         </>
     )
 }
