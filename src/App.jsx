f17576a7734d17caca1ee329ed53d2e79140b804
Udhayakumar M <udhayakumar2969@gmail.com>
2026-07-16 10:18:45 +0530

    appfile changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,7 @@ function App() {
     <Home 
     name= "Kannan"
     age={22}
-<<<<<<< HEAD
-=======
     mobile={9872634357}
->>>>>>> c35dc323fbbdaf83e2e07cebf7d992efdd09060b
     city ="Covai" 
     />
 
